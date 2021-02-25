2f1781224b5f8aa4495195f00a6b645324e0492b
Mike Kelly <mkelly32@tcd.ie>
2021-02-25 12:05:03 +0000

    project initialized

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    state = { loading: false };
+
+    componentDidMount() {
+        // 测试 devServer 的代理功能
+        // fetch('/api/category')
+        //     .then(resp => resp.json())
+        //     .then(res => console.log('here here', res));
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Welcome to React</h2>
+                </div>
+                <p>{ this.state.loading.toString() }</p>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
+}
+
+export default App;
