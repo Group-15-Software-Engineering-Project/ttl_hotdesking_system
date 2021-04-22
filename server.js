d34ce858c2013f7f7557222b2169ff99635e0b28
aaronByrne1 <65355719+aaronByrne1@users.noreply.github.com>
2021-04-22 12:26:10 +0100

    Added quicksort to sort most active user

--- server.js
+++ server.js
@@ -444,6 +444,9 @@ function getMostActiveUser(data) {
   if (labels.length === 1) {
     amountOfBookings.push(data.length);
   }
+  arr=quickSort(labels, amountOfBookings,0,labels.length-1);
+  labels=arr[0];
+  amountOfBookings=arr[1];
   return [labels, amountOfBookings];
 }
 
@@ -1277,6 +1280,51 @@ function deleteUser(email) {
       }
     });
   });
+}
+
+function swap(labels,items, leftIndex, rightIndex){
+  var temp = items[leftIndex];
+  var temp2=labels[leftIndex];
+
+  items[leftIndex] = items[rightIndex];
+  labels[leftIndex] =labels[rightIndex];
+
+  labels[rightIndex]=temp2;
+  items[rightIndex] = temp;
+}
+function partition(labels,items, left, right) {
+  var pivot   = items[Math.floor((right + left) / 2)], //middle element
+      i       = left, //left pointer
+      j       = right; //right pointer
+  while (i <= j) {
+      while (items[i] < pivot) {
+          i++;
+      }
+      while (items[j] > pivot) {
+          j--;
+      }
+      if (i <= j) {
+          swap(labels,items, i, j); //sawpping two elements
+          i++;
+          j--;
+      }
+  }
+  return i;
+}
+
+function quickSort(labels,items, left, right) {
+  var index;
+  if (items.length > 1) {
+      index = partition(labels,items, left, right); //index returned from partition
+      if (left < index - 1) { //more elements on the left side of the pivot
+          quickSort(labels,items, left, index - 1);
+      }
+      if (index < right) { //more elements on the right side of the pivot
+          quickSort(labels,items, index, right);
+      }
+  }
+  
+  return [labels,items];
 }
 //Environment
 
