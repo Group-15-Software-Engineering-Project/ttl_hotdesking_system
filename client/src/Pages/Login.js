21dc102977f507527b03121bb73df55841ce9249
GuzowskD <guzowskd@tcd.ie>
2021-08-29 17:05:27 +0100

    Fixed account page

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
-import { Form } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
 import "../public/css/Login.css";
 import "../public/css/main.css";
-import { setSessionToken, verify } from "../Components/Misc";
+import { verify } from "../Components/Misc";
 import { Redirect } from "react-router-dom";
 import TCDLogo from "../public/media/TCD-logo-home-transparent.png";
 
@@ -32,34 +30,15 @@ class Login extends Component {
         return notifs;
     };
 
-    getNotifications = () => {
-        fetch("/api/getNotifications")
-            .then((res) => {
-                return res.json();
-            })
-            .then((res) => {
-                if (res.error) {
-                    alert("Could not load Notifications");
-                } else {
-                    let data = this.transformNotifications(res.notifications);
-                    sessionStorage.setItem("notifications", JSON.stringify({ data }));
-                }
-            })
-            .catch((err) => {
-                alert(err);
-            });
-    };
-
     componentDidMount = () => {
         window.addEventListener("keydown", (e) => {
             if (e.key === "Enter" && this.state.password.length > 0) {
                 this.submitLogin();
             }
         });
-        if (!(verify(true) || verify(false))) {
-            sessionStorage.clear();
-        }
+        sessionStorage.clear();
     };
+
     submitLogin = () => {
         fetch("/api/login", {
             method: "POST",
@@ -150,80 +129,6 @@ class Login extends Component {
                     </div>
                 </div>
             </div>
-            // <div>
-            //     <img
-            //         id="clip"
-            //         src={TCDLogo}
-            //         alt="TCD Logo"
-            //         className="login-logo"
-            //         style={{ zIndex: "6" }}
-            //     />
-            //     <div className="block-top">
-            //         <div className="title-text">Trinity Teaching & Learning Desk Booking</div>
-            //     </div>
-            //     <div className="wrapper TCD">
-            //         <div className="flex-container-1" />
-            //         <div
-            //             className="flex-container-5"
-            //             style={{
-            //                 alignText: "center",
-            //                 paddingTop: "100px",
-            //                 boxShadow: "0 0 5px #333",
-            //                 backgroundColor: "white",
-            //             }}>
-            //             <Form>
-            //                 <Form.Group size="lg" controlId="email">
-            //                     <Form.Control
-            //                         className="text-field"
-            //                         placeholder="Email"
-            //                         autoFocus
-            //                         type="email"
-            //                         value={this.state.email}
-            //                         onChange={(e) => this.setState({ email: e.target.value })}
-            //                     />
-            //                 </Form.Group>
-            //                 <Form.Group size="lg" controlId="password">
-            //                     <Form.Control
-            //                         className="text-field"
-            //                         placeholder="Password"
-            //                         type="password"
-            //                         value={this.state.password}
-            //                         onChange={(e) =>
-            //                             this.setState({ password: e.target.value })
-            //                         }
-            //                     />
-            //                 </Form.Group>
-
-            //                 <Button className="login-button" onClick={this.submitLogin}>
-            //                     Login
-            //                 </Button>
-            //                 {this.state.errorText ? (
-            //                     <>
-            //                         <br />
-            //                         <span style={{ color: "red", marginLeft: "-1.5px" }}>
-            //                             {"Email or password did not match."}
-            //                         </span>
-            //                     </>
-            //                 ) : null}
-            //             </Form>
-            //         </div>
-            //         <div className="flex-container-1" />
-            //     </div>
-            //     <footer
-            //         style={{
-            //             backgroundColor: "#717171",
-            //             height: "10vh",
-            //             zIndex: "3",
-            //             color: "white",
-            //             textAlign: "center",
-            //             font: "Calibri",
-            //             paddingTop: "2vh",
-            //         }}>
-            //         {
-            //             "For issues regarding authentication, please contact: insert.email@here.ie"
-            //         }
-            //     </footer>
-            // </div>
         );
     }
 }
